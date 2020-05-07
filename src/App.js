8bfa093d3847be07e42041e1ff5f5a6b903ad47c
Rex Ye <rxye1991@gmail.com>
2020-05-07 18:29:42 -0400

    addeduserstories

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
     <div className="App">
         <h4>EDM Community Site</h4>
         <p>User stories: Login, profile, follow, followers, upload image, upload favourite youtube vidoes embeeded.</p>
+      
     </div>
   );
 }
