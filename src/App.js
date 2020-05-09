3d1000694f5af769ee3fa8487c50ace09c6cd13a
Rex Ye <rxye1991@gmail.com>
2020-05-09 14:55:51 -0400

    addedLandingpagecomponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import LandingPage from './Components/LandingPage/LandingPage'
 
 const App = () => {
   return (
     <div className="App">
-        <h4>EDM Community Site</h4>
-        <p>User stories: Login, profile, follow, followers, upload image, upload favourite youtube vidoes embeeded.</p>
-      
+        <LandingPage />
     </div>
   );
 }
---
+++ src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+export class LandingPage extends Component{
+    render(){
+        return(
+            <div>
+                <h4>EDM Community Site</h4>
+                <h1>Hello World </h1>
+            </div>
+        )
+    }
+}
+
+export default LandingPage 
