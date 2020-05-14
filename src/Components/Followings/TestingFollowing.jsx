5dc47878eed8b4fec5c4c24a98041412597d3399
Rex Ye <rxye1991@gmail.com>
2020-05-14 18:05:00 -0400

    creatingtestingfollowingfeaturecomponent

---
+++ src/Components/Followings/TestingFollowing.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class TestingFollowing extends Component{
+    render(){
+        return(
+            <div>
+                <h4>Testing Following Feature</h4>
+            </div>
+        )
+    }
+}
+
+export default TestingFollowing 
