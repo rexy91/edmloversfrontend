a1698e47a4884e6711ff81e193e47affaf459272
Rex Ye <rxye1991@gmail.com>
2020-05-17 21:45:20 -0400

    addeduserloginaction

--- src/Components/Redux/followReducer.js
+++ src/Components/Redux/followReducer.js
@@ -6,7 +6,12 @@ export default function followReducer(state = initialState, action){
     //Switch statement here, action has two arguments, (type, payload)
     switch(action.type){
 
-       case 'testing': {}
+       case 'testing': return {}
+
+       case 'USER_LOGIN':
+           return {
+               
+           }
 
        default : {}
     }
