a27cf43497978027a09c03ada55757a43e9f7b6f
Rex Ye <rxye1991@gmail.com>
2020-05-19 18:38:27 -0400

    createdModallogincomponent

---
+++ src/Components/Login/ModalLogin.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+class Login extends Component(){
+
+    render(){
+        return(
+            <div>
+                    <h1>Modal Login</h1>
+            </div>
+        )
+    }
+}
+
+export default Login 
