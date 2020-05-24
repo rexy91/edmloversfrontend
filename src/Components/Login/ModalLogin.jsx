a08a0adb03206b5ee074cec1e715f5e79adaa935
Rex Ye <rxye1991@gmail.com>
2020-05-24 19:48:18 -0400

    ModalLoginFormHTMLdone

--- src/Components/Login/ModalLogin.jsx
+++ src/Components/Login/ModalLogin.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { Button, Header, Image, Modal, Form} from 'semantic-ui-react'
+import { Button, Header, Image, Modal, Form, Checkbox} from 'semantic-ui-react'
 
 
 const ModalLogin = () => (
   <Modal trigger={<Button>Login</Button>} centered={false}>
-    <Modal.Header>Select a Photo</Modal.Header>
+    <Modal.Header>Please Login</Modal.Header>
     <Modal.Content>
-
+        <Form>
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder='First Name' />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder='Last Name' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='I agree to the Terms and Conditions' />
+        </Form.Field>
+        <Button type='submit'>Submit</Button>
+      </Form>
     </Modal.Content>
   </Modal>
 )
