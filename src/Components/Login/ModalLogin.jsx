173a296678b3cd8c17e5bdca03fdb0a46c4613c1
Rex Ye <rxye1991@gmail.com>
2020-05-30 15:56:31 -0400

    controlledloginFunctionalComponentForm

--- src/Components/Login/ModalLogin.jsx
+++ src/Components/Login/ModalLogin.jsx
@@ -8,11 +8,7 @@ const ModalLogin = () => {
   const [userName, setUsername] = useState('')
   const [passWord, setPassword] = useState('')
 
-  const handleOnchange = (e) => {
-        // Controlling the username input
-        setUsername(e.value)
-  }
-
+  console.log(userName,passWord)
   return(
   <Modal trigger={<Button>Login</Button>} centered={false}>
     <Modal.Header>Please Login</Modal.Header>
@@ -20,12 +16,11 @@ const ModalLogin = () => {
         <Form>
         <Form.Field>
           <label>Username</label>
-
-          <input placeholder='Username' name='username' onChange={handleOnchange} value={userName} />
+          <input placeholder='Username' name='username' onChange={ e => setUsername(e.target.value)} value={userName} />
         </Form.Field>
         <Form.Field>
           <label>Password</label>
-          <input placeholder='Enter password' name='password' onChange={handleOnchange} value={passWord}  />
+          <input placeholder='Enter password' name='password' onChange={e => setPassword(e.target.value)} value={passWord}  />
         </Form.Field>
         <Form.Field>
           <Checkbox label='I agree to the Terms and Conditions' />
