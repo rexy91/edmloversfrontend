721f0348302e3e19b87693aab280d67ecadbce69
Rex Ye <rxye1991@gmail.com>
2020-06-28 18:15:42 -0400

    settingupRedux

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+import LandingPage from './Components/LandingPage/LandingPage'
+
+class App extends Component{
+  
+  render(){
+      return(
+        <div>
+          <LandingPage/>
+        </div>
+      )
+  }
+}
+
+
+export default App
