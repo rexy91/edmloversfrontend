5ef1332b7dfe5f8a1546d3ceb4c0c00ecfaeec68
Rex Ye <rxye1991@gmail.com>
2020-06-30 18:45:57 -0400

    exportFollowReducer,initalizedState

--- src/Redux/followReducers.js
+++ src/Redux/followReducers.js
@@ -2,4 +2,14 @@ const initialState={}
 
 const followReducer = (state = initialState, action) => {
 
+    // switch(action.type){
+
+    //     case 'example':
+    //         return {}
+        
+    //     default: return {} 
+    // }
+
 }
+
+export default followReducer
