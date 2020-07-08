4a5339cc44ab279a65e04db6757996a5284920dd
Rex Ye <rxye1991@gmail.com>
2020-07-08 13:49:07 -0400

    createdProfileComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
 import './App.css';
 import LandingPage from './Components/LandingPage/LandingPage'
+import {Switch,Route,Link} from "react-router-dom";
+
+
+// Components
+import Profilepage from './Components/Profile/Profilepage'
 
 class App extends Component{
   
   componentDidMount(){
       
   }
+
   render(){
       return(
         <div>
           <LandingPage/>
+          <Switch>
+              <Route path='/profile/:id' component={ Profilepage } />
+          </Switch>
         </div>
       )
   }
---
+++ src/Components/Profile/Profilepage.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Profilepage extends Component{
+
+    render(){
+        return(
+            <div>
+                <h3>Profile Page</h3>
+            </div>
+        )
+    }
+}
+
+export default Profilepage
