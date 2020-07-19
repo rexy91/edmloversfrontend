9ff258b10b4f7905b8323bb9da40d3ecfb0e62d7
Rex Ye <rxye1991@gmail.com>
2020-07-19 17:29:57 -0400

    ableToStoreChosenFilestolocalstate

--- src/Components/Profile/NewPost/NewPost.js
+++ src/Components/Profile/NewPost/NewPost.js
@@ -1,26 +1,46 @@
 import React from 'react'
 import { Button, Header, Icon, Image, Modal } from 'semantic-ui-react'
 import newpostGif from '../../../photos/newpost.gif'
-const NewPost = () => (
-  <Modal trigger={<Button>New Post</Button>}>
-    <Modal.Header>Make a post</Modal.Header>
-    <Modal.Content image>
-      <Image wrapped size='medium' src={newpostGif} />
-      <Modal.Description>
-        <Header>Modal Header</Header>
-        <p>
-          This is an example of expanded content that will cause the modal's
-          dimmer to scroll
-        </p>
-        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png'/>
-      </Modal.Description>
-    </Modal.Content>
-    <Modal.Actions>
-      <Button primary>
-        Proceed <Icon name='right chevron' />
-      </Button>
-    </Modal.Actions>
-  </Modal>
-)
+import {useState} from 'react'
+import './Newpost.scss'
+
+const NewPost = () => {
+    
+    const [photos, setPhotos] = useState([])
+    
+
+    const handlePhotoUpload = (e) => {
+            setPhotos(e.target.files)
+            // Here photos will still be empty, but outside this function photo will be updated.
+            console.log(photos)
+    }
+    
+    const handleChosenPhotos = () => {
+        // e.target.file is a file lists, not an array, but we can spread and store in an array.
+        // With ES6 syntax.
+        
+    }
+    return(
+        <Modal trigger={<Button>New Post</Button>} centered={false} class='newpostModal'>
+            <Modal.Header>Post something!</Modal.Header>
+            <Modal.Content image>
+            <Image wrapped size='medium' src={newpostGif} />
+            <Modal.Description>
+                <Header>Modal Header</Header>
+                <p id='111'>Content</p>
+                <textarea rows={6} cols={60}></textarea>
+                <br/>
+                <input onChange={handlePhotoUpload} type="file" multiple="multiple" name="file" id="file" class="inputfile" />
+                <label for="file"><img id='addPhotoButton' src='https://img.icons8.com/officel/2x/add-image.png'></img></label>
+            </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+            <Button primary>
+                Proceed <Icon name='right chevron' />
+            </Button>
+            </Modal.Actions>
+        </Modal>
+  )
+}
 
 export default NewPost
