827a1a854bdf63d5e38de02364a6817be8c3cf0d
Rex Ye <rxye1991@gmail.com>
2020-07-22 13:18:27 -0400

    createdProfileiconandeditbutton

--- src/Components/Profile/Profilepage.js
+++ src/Components/Profile/Profilepage.js
@@ -3,18 +3,23 @@ import {useSelector} from 'react-redux'
 import NewPost from './NewPost/NewPost'
 import '../Profile/Profilepage.scss'
 
+import testProfilePic from '../../photos/IMG_3928.jpg'
+
 export default function Profilepage() {
 
     // Agrument of the callback is the current Redux state, just like mstp.
     // const user = useSelector(state => console.log(state.userReducer))
 
      return (
-        <div>
+        <div className='profilePage'>
             <h3>Profile Page</h3>
             <NewPost/>
             <div className='container1'>
                     <div className='box1'>
-                            <p>sdfljdsljglsdjfjdslgjsdgjfdsjfds</p>
+                            <button>Edit Profile</button>
+                            <img id='profilePicIcon'
+                            src={testProfilePic}/>
+                            <p>fjfjfjsjfjsdjsdf</p>
                     </div>
                     <div className='box2'>
                             <p>sdfljdsljglsdjfjdslgjsdgjfdsjfds</p>
@@ -23,6 +28,7 @@ export default function Profilepage() {
         </div>
     )
 }
+
 
 
 
